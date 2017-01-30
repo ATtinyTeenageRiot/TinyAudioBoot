0c5f38eb08d66bd81cb95a6e6b981761b3a1e3f5
Budi Prakosa <iyok@deadmediafm.org>
2017-01-30 12:50:44 +0700

    refactor, failed

--- TinyAudioBoot/TinyAudioBoot.ino
+++ TinyAudioBoot/TinyAudioBoot.ino
@@ -3,16 +3,14 @@
 #include <stdlib.h>
 #include <avr/boot.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
 
 #define ARDUINO_DEBUG_SERIAL
-#define TOP_ADDRESS 0x1BFF
 
 #ifdef ARDUINO_DEBUG_SERIAL
 #include <SendOnlySoftwareSerial.h>
 SendOnlySoftwareSerial mySerial (PB2);  // Tx pin
 #endif
-
-uint8_t prog_count = 0;
 
 #define LEDPORT (1<<PB1); //PB1 pin 6 Attiny85
 #define INITLED {DDRB|=LEDPORT;}
@@ -35,7 +33,6 @@ uint8_t prog_count = 0;
 #define true (1==1)
 #define false !true
 
-
 //***************************************************************************************
 // main loop
 //***************************************************************************************
@@ -44,13 +41,13 @@ uint8_t prog_count = 0;
 
 // frame format definition
 #define COMMAND         0
-#define PAGEINDEXLOW   1  // page address lower part
+#define PAGEINDEXLOW    1  // page address lower part
 #define PAGEINDEXHIGH   2  // page address higher part
 #define CRCLOW          3  // checksum lower part 
-#define CRCHIGH     4  // checksum higher part 
+#define CRCHIGH         4  // checksum higher part 
 #define DATAPAGESTART   5  // start of data
 #define PAGESIZE    SPM_PAGESIZE
-#define FRAMESIZE       (PAGESIZE+DATAPAGESTART)// size of the data block to be received
+#define FRAMESIZE   (PAGESIZE+DATAPAGESTART) // size of the data block to be received
 
 // bootloader commands
 #define NOCOMMAND       0
@@ -59,6 +56,36 @@ uint8_t prog_count = 0;
 #define RUNCOMMAND      3
 
 uint8_t FrameData[FRAMESIZE];
+
+// #define RJMP (0xC000U - 1) // opcode of RJMP minus offset 1
+// #define RESET_SECTION __attribute __ ((section ("bootreset")))
+
+// Microcontroller vectortable entries in the flash
+#define RESET_VECTOR_OFFSET         0
+// number of bytes before the boot loader vectors to store the tiny application vector table
+#define TINYVECTOR_RESET_OFFSET     4
+#define TINYVECTOR_OSCCAL_OFFSET    6
+/* ------------------------------------------------------------------------ */
+// postscript are the few bytes at the end of programmable memory which store tinyVectors
+#define POSTSCRIPT_SIZE 6
+#define PROGMEM_SIZE (BOOTLOADER_ADDRESS - POSTSCRIPT_SIZE) /* max size of user program */
+
+#define FLASH_RESET_ADDR  0x0000                      // address of reset vector (in bytes)
+#define BOOTLOADER_STARTADDRESS BOOTLOADER_ADDRESS    // start address:
+#define BOOTLOADER_ENDADDRESS   0x2000                // end address:   0x2000 = 8192
+#define LAST_PAGE (BOOTLOADER_STARTADDRESS - SPM_PAGESIZE)/SPM_PAGESIZE
+
+#define RJMP    (0xC000U - 1)
+
+typedef union {
+uint16_t w;
+uint8_t b[2];
+} uint16_union_t;
+
+register uint16_union_t currentAddress  asm("r4");  // r4/r5 current progmem address, used for erasing and writing 
+
+uint16_t saved_reset_vector;
+uint8_t prog_count = 0;
 
 //***************************************************************************************
 // receiveFrame()
@@ -168,7 +195,6 @@ uint8_t receiveFrame()
 void boot_program_page (uint32_t page, uint8_t *buf)
 {
   uint16_t i;
-
   cli(); // disable interrupts
 
   boot_page_erase (page);
@@ -176,8 +202,23 @@ void boot_program_page (uint32_t page, uint8_t *buf)
 
   for (i = 0; i < SPM_PAGESIZE; i += 2)
   {
-    uint16_t w = *buf++;
-    w += (*buf++) << 8;
+    //read received data
+    uint16_t w = *buf++; //low section
+    w += (*buf++) << 8; //high section
+    //combine low and high to get 16 bit
+
+    // //first page and first index is vector table... ( page 0 and index 0 )
+    // if (page == 0 && i == 0)
+    // {
+    //     //1.save jump to application vector for later patching      
+    //     uint16_t saved_reset_vector = (w - RJMP);
+    //     //2.replace w with jump vector to bootloader        
+    //     w = 0xC000 + (BOOTLOADER_ADDRESS/2) - 1;
+    // }else if (page == LAST_PAGE && i == 60)
+    // {
+    //   //3.retrieve saved reset vector
+    //   w = saved_reset_vector;
+    // }
 
     boot_page_fill (page + i, w);
     boot_spm_busy_wait();       // Wait until the memory is written.
@@ -186,7 +227,6 @@ void boot_program_page (uint32_t page, uint8_t *buf)
   boot_page_write (page);     // Store buffer in flash page.
   boot_spm_busy_wait();       // Wait until the memory is written.
 
-  //boot_rww_enable ();
 }
 //***************************************************************************************
 void initstart()
@@ -196,21 +236,15 @@ void initstart()
   TCCR0B = _BV(CS01);
 }
 //***************************************************************************************
-typedef void (*fptr_t)(void);
+
 
 void runProgramm(void)
 {
-
   // reintialize registers to default
   DDRB = 0;
   cli();
   TCCR0B = 0; // turn off timer1
-
-    fptr_t reset = (fptr_t) 0;
-    
-    reset();
-
-
+  asm volatile ("rjmp __vectors - 4"); // jump to application reset vector at end of flash
 }
 
 //***************************************************************************************
