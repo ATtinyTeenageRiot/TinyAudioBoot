3340a6ba86dae4af1d865c627d133e4ea29b2069
Budi Prakosa <iyok@deadmediafm.org>
2017-03-11 09:12:57 +0700

    script for building jar

--- java_source/java_compile.sh
+++ java_source/java_compile.sh
@@ -1,2 +1,5 @@
+rm -fr */*.class
 javac -classpath . */*.java
-java -cp . controllPanel/Main_WavBootLoader
+jar cvfm hex2wav.jar MANIFEST.MF */*.class
+#java -cp . controllPanel/Main_WavBootLoader
+java -jar hex2wav.jar /Users/xcorex/Documents/Projects/AVR/8BitMixtapeNeo/TinyAudioBoot/build/AudioBootAttiny85_AudioPB3_LEDPB1.hex
