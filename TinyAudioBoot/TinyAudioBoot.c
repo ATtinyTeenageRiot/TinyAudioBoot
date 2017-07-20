adfb75b02e39476292c184d38fab4aeaae4a152b
Budi Prakosa <iyok@deadmediafm.org>
2017-07-20 07:34:59 +0700

    led off after eeprom

--- TinyAudioBoot/TinyAudioBoot.c
+++ TinyAudioBoot/TinyAudioBoot.c
@@ -648,7 +648,7 @@ static inline void a_main()
             //Leave bootloader after eeprom signal received
             //todo: wait until all data sent > spm pagesize (64)
             //fix this!!!!
-
+            LEDOFF;
             exitBootloader();
 
         }
